88a3c6d25ad97218cbfcd5a6a2f0f292098906db
Ray Chen <b06901135@ntu.edu.tw>
2021-12-12 15:04:45 +0800

    Add asv.py

---
+++ utils/install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# for asv.py
+pip install resemblyzer matplotlib umap-learn
+
+# for mos.py
+pip install s3prl
+pip install fairseq@git+https://github.com//pytorch/fairseq.git@f2146bdc7abf293186de9449bfa2272775e39e1d#egg=fairseq
+
