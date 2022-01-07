a2c6e38093460ea709ef4d606dc1145be91a571d
Ray Chen <b06901135@ntu.edu.tw>
2022-01-07 18:34:26 +0800

    Update eval script for ifgsm mifgsm pgd

--- scripts/eval_vctk.sh
+++ scripts/eval_vctk.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-EXP_NAME="vctk_white_box"
+DATA_ROOT="/hdd0/CORPORA/VCTK-Corpus"
 
+EXP_NAME="vctk_white_box_ifgsm"
 echo "EXP_NAME = ${EXP_NAME}"
-python3 eval_attack.py --checkpoint_path pretrained_model/meta_stylespeech.pth.tar --save_dir results/${EXP_NAME}
+python3 eval_attack.py --data_root $DATA_ROOT --checkpoint_path pretrained_model/meta_stylespeech.pth.tar --save_dir results/${EXP_NAME}
+python3 eval_agent.py results/${EXP_NAME}
+
+EXP_NAME="vctk_white_box_mifgsm"
+echo "EXP_NAME = ${EXP_NAME}"
+python3 eval_attack.py --data_root $DATA_ROOT --checkpoint_path pretrained_model/meta_stylespeech.pth.tar --save_dir results/${EXP_NAME} --momentum 1
+python3 eval_agent.py results/${EXP_NAME}
+
+EXP_NAME="vctk_white_box_pgd"
+echo "EXP_NAME = ${EXP_NAME}"
+python3 eval_attack.py --data_root $DATA_ROOT --checkpoint_path pretrained_model/meta_stylespeech.pth.tar --save_dir results/${EXP_NAME} --random_start
 python3 eval_agent.py results/${EXP_NAME}
 
